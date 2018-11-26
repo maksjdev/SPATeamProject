59613a51305c87a9b2b62897e3198fe58a58fbb4
Viacheslav <yamteee@gmail.com>
2018-11-26 17:16:42 +0200

    Some fixes

--- src/app/main/components/block-components/full-news-block/full-news-block.component.ts
+++ src/app/main/components/block-components/full-news-block/full-news-block.component.ts
@@ -1,27 +1,40 @@
-import {Component, Input, OnInit, SimpleChanges} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {News} from '@shared/models/News';
 import {MockDataService} from '@shared/mock-data.service';
 import {Comment} from '@shared/models/Comment';
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-full-news-block',
   templateUrl: './full-news-block.component.html',
-  styleUrls: ['./full-news-block.component.scss']
+  styleUrls: ['./full-news-block.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FullNewsBlockComponent implements OnInit {
+export class FullNewsBlockComponent implements OnChanges{
   @Input() news: News;
+  public _htmlContent: string;
+
   favorites: boolean;
   commentsList: Array<Comment>;
 
   constructor(
-    private mockService: MockDataService
-  ) {}
+    private mockService: MockDataService,
+    private _sanitizer: DomSanitizer
+  ) { }
 
-  ngOnInit(): void {
-    this.commentsList = this.mockService.getMockCommentList(5);
+  toggle(event) {
+    this.favorites = !this.favorites;
   }
 
-  toggle(event){
-    this.favorites = !this.favorites;
+  public get htmlContent(): SafeHtml {
+    let string = this._htmlContent ? this._htmlContent : '';
+    return this._sanitizer.bypassSecurityTrustHtml(string);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.hasOwnProperty('news') && changes['news'].currentValue){
+      this._htmlContent = this.news.text;
+      this.commentsList = this.mockService.getMockCommentList(5);
+    }
   }
 }
